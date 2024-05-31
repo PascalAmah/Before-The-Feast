1b610eaee6c8257a64be00e2eb819a7e08894807
Pascal Amaliri <pascal.amah10@gmail.com>
2024-05-31 05:42:42 +0100

    My files

---
+++ app.js
@@ -0,0 +1,38 @@
+
+const daysCount = document.getElementById('days');
+
+const hoursCount = document.getElementById('hours');
+
+const minsCount = document.getElementById('mins');
+const secondsCount = document.getElementById('seconds');
+
+const babyBirthday = "7 June 2024";
+
+function countdown() {
+    const birthDay = new Date(babyBirthday);
+    const currentDate = new Date;
+
+    const totalSeconds = (birthDay - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysCount.innerHTML = days;
+    hoursCount.innerHTML = formatTime(hours);
+    minsCount.innerHTML = formatTime(mins);
+    secondsCount.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+// initial call
+countdown();
+
+setInterval(countdown, 1000);
+
+//document.querySelector('.countdown-container').style.width = '700px';
+
